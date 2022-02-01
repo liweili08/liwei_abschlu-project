7c7835385f00b33dc51bf324188acff694050e68
liwei <liweili@qq.com>
2022-02-01 22:11:13 +0100

    HomePage

---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,16 @@
+
+
+export default function HomePage(){
+    return(
+        <div className="homePage">
+            <h1> Herzlich Willkommen zu XXXXXXX </h1>
+            <h4 className="info"> ein bisschen erklären, wie man Mathe durch das App lernen kann </h4>
+            <h2> Wählen Sie ein Thema: </h2>
+            <ul className="themenListe">
+                <li> Folgen </li>
+                <li> Reinen </li>
+                <li> ... </li>
+            </ul>
+        </div>
+    )
+}
