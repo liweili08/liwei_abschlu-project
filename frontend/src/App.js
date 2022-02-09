7999a2dae0c84ffcf493eff934d9d20bbc0c5a04
liwei <liweili@qq.com>
2022-02-09 15:14:33 +0100

    test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,18 +7,30 @@ import Beispiel from "./pages/Beispiel";
 import AufgabenGesamt from "./pages/AufgabenGesamt";
 import Aufgabe from "./pages/Aufgabe";
 import Ergebnis from "./pages/Ergebnis";
+import {BrowserRouter, Route,Routes} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-     {/*<HomePage/>*/}
-        {/*<Thema/>*/}
-       {/* <Grundlagen/>*/}
-       {/* <Beispiel/>*/}
-       {/* <AufgabenGesamt/>*/}
-      {/*<Aufgabe/>*/}
-        <Ergebnis/>
-    </div>
+    // <div className="App">
+   //  {/*<HomePage/>*/}
+    //    {/*<Thema/>*/}
+    //   {/* <Grundlagen/>*/}
+    //   {/* <Beispiel/>*/}
+    //   {/* <AufgabenGesamt/>*/}
+     // {/*<Aufgabe/>*/}
+     // {/*  <Ergebnis/>*/}
+   // {/*</div>*/}
+      <BrowserRouter>
+          <Routes>
+              <Route path="/*" element={<HomePage/>}/>
+              <Route path="/folgen" element={<Thema/>}/>
+              <Route path="/reihen" element={<Thema/>}/>
+          </Routes>
+      </BrowserRouter>
+
+     /* <div>
+          <HomePage/>
+      </div>*/
   );
 }
 
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,3 +1,5 @@
+import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
+import Thema from "./Thema";
 
 
 export default function HomePage(){
@@ -7,10 +9,18 @@ export default function HomePage(){
             <h4 className="info"> ein bisschen erklären, wie man Mathe durch das App lernen kann </h4>
             <h2> Wählen Sie ein Thema: </h2>
             <ul className="themenListe">
-                <li> Folgen </li>
-                <li> Reinen </li>
-                <li> ... </li>
+                <li><Link to="folgen">Folgen </Link> </li>
+                <li><Link to="reihen"> Reinen</Link> </li>
+                <li>Folgen </li>
             </ul>
+
+           {/* <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<HomePage/>}/>
+                    <Route path="themen" element={<Thema/>}/>
+                </Routes>
+            </BrowserRouter>*/}
+
         </div>
     )
 }
